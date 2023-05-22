1d49b9de7b233335f4a1dbdc0624eec1eefe4ac0
SfShev <pshev1406@gmail.com>
2023-05-22 19:12:22 +0300

    graphics new

--- src/main/java/org/example/Display/Display.java
+++ src/main/java/org/example/Display/Display.java
@@ -2,6 +2,9 @@ package org.example.Display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+import java.util.Arrays;
 
 public class Display {
 
@@ -9,36 +12,49 @@ public class Display {
     private static JFrame window;
     private static Canvas content;
 
-    public static void create ( int width, int height, String title){
-        if (created){
+    private static BufferedImage buffer;
+    private static int[] bufferData;
+    private static Graphics bufferGraphics;
+    private static int clearColor;
+
+    public static void create(int width, int height, String title, int _clearColor) {
+        if (created) {
             return;
         }
         window = new JFrame(title);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        content = new Canvas(){
-          public void paint (Graphics g){
-              super.paint(g);
-              render(g);
-          }
-        };
+        content = new Canvas();
 
-        Dimension size = new Dimension(width,height);
+        Dimension size = new Dimension(width, height);
         content.setPreferredSize(size);
-        content.setBackground(Color.BLACK);
+
 
         window.setResizable(false);
         window.getContentPane().add(content);
         window.pack();
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
+        bufferGraphics = buffer.getGraphics();
+        clearColor = _clearColor;
+
+        created = true;
+    }
+    public static void clear(){
+        Arrays.fill(bufferData,clearColor);
     }
 
     public static void render(){
-        content.repaint();
-    }
-    private static void render (Graphics g){
-        g.setColor(Color.WHITE);
-        g.fillOval(400 - 50,400-50,100,100);
-    }
+        bufferGraphics.setColor(new Color(0xff0000ff));
+        bufferGraphics.fillOval(350,250,100,100);
 
+
+    }
+    public static void swapBuffers(){
+        Graphics g = content.getGraphics();
+        g.drawImage(buffer,0,0,null);
+
+    }
 }
