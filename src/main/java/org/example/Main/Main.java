1cebcbed31d14dadbef8a0e5a282f0a8d3c862d9
SfShev <pshev1406@gmail.com>
2023-05-22 17:06:59 +0300

    graphics test

--- src/main/java/org/example/Main/Main.java
+++ src/main/java/org/example/Main/Main.java
@@ -2,8 +2,20 @@ package org.example.Main;
 
 import org.example.Display.Display;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
 public class Main {
     public static void main(String[] args) {
         Display.create(800,800,"Tanks");
+
+        Timer t = new Timer(1000/60, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Display.render();
+            }
+        });
+        t.setRepeats(true);
+        t.start();
     }
 }
