16bd22fbbdf9dfb989d509a57612c1100b3536b2
SfShev <pshev1406@gmail.com>
2023-05-23 23:13:47 +0300

    Structure of the game

--- src/main/java/org/example/Display/Display.java
+++ src/main/java/org/example/Display/Display.java
@@ -2,6 +2,7 @@ package org.example.Display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferInt;
 import java.util.Arrays;
@@ -16,8 +17,9 @@ public class Display {
     private static int[] bufferData;
     private static Graphics bufferGraphics;
     private static int clearColor;
+    private static BufferStrategy bufferStrategy;
 
-    public static void create(int width, int height, String title, int _clearColor) {
+    public static void create(int width, int height, String title, int _clearColor,int numBufers) {
         if (created) {
             return;
         }
@@ -38,7 +40,11 @@ public class Display {
         buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         bufferData = ((DataBufferInt) buffer.getRaster().getDataBuffer()).getData();
         bufferGraphics = buffer.getGraphics();
+        ((Graphics2D) bufferGraphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
         clearColor = _clearColor;
+
+        content.createBufferStrategy(numBufers);
+        bufferStrategy = content.getBufferStrategy();
 
         created = true;
     }
@@ -46,15 +52,23 @@ public class Display {
         Arrays.fill(bufferData,clearColor);
     }
 
-    public static void render(){
-        bufferGraphics.setColor(new Color(0xff0000ff));
-        bufferGraphics.fillOval(350,250,100,100);
 
-
-    }
     public static void swapBuffers(){
-        Graphics g = content.getGraphics();
+        Graphics g = bufferStrategy.getDrawGraphics();
         g.drawImage(buffer,0,0,null);
-
+        bufferStrategy.show();
     }
+
+    public static Graphics2D getGraphics(){
+        return (Graphics2D) bufferGraphics;
+    }
+
+    public static void destroy(){
+        if(!created) return;
+        window.dispose();
+    }
+    public static void setTitle(String title){
+        window.setTitle(title);
+    }
+
 }
---
+++ src/main/java/org/example/Game/Game.java
@@ -0,0 +1,109 @@
+package org.example.Game;
+
+import org.example.Display.Display;
+import org.example.Utils.Time;
+
+public class Game implements Runnable {
+
+
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final String TITLE = "Tanks";
+    public static final int CLEAR_COLOR = 0xff000000;
+    public static final int NUM_BUFFERS = 3;
+
+    public static final float UPDATE_RATE = 60.0f;
+    public static final float UPDATE_INTERVAL = Time.SECOND / UPDATE_RATE;
+    public static final long IDLE_TIME = 1;
+
+    private boolean running;
+    public Thread gameThread;
+
+    public Game() {
+        running = false;
+        Display.create(WIDTH, HEIGHT, TITLE, CLEAR_COLOR, NUM_BUFFERS);
+    }
+
+    public synchronized void Start() {
+        if (running) return;
+        running = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public synchronized void Stop() {
+        if (!running) return;
+        running = false;
+
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        CleanUp();
+    }
+
+    private void Update() {
+
+    }
+
+    private void Render() {
+        Display.clear();
+        Display.swapBuffers();
+    }
+
+    @Override
+    public void run() {
+        int fps = 0;
+        int upd = 0;
+        int updl = 0;
+
+        long count = 0;
+
+        float delta = 0;
+        long lastTime = Time.get();
+        while (running) {
+            long now = Time.get();
+            long elapsedTime = now - lastTime;
+            lastTime = now;
+
+            count += elapsedTime;
+
+            boolean render = false;
+
+            delta += (elapsedTime / UPDATE_INTERVAL);
+            while (delta > 1) {
+                Update();
+                upd++;
+                delta--;
+                if (render) {
+                    updl++;
+                } else {
+                    render = true;
+                }
+            }
+            if (render) {
+                Render();
+                fps++;
+            } else {
+                try {
+                    Thread.sleep(IDLE_TIME);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (count >= Time.SECOND) {
+                Display.setTitle(TITLE + " || fps: " + fps + " | upd: " + upd + " | updl: " + updl);
+                upd = 0;
+                fps = 0;
+                updl = 0;
+                count = 0;
+
+            }
+        }
+    }
+
+    private void CleanUp() {
+        Display.destroy();
+    }
+}
